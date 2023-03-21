793165e3002ac84203b6ebaa41b87f5e742922ad
Toan Le <toanle@Toans-MacBook-Air.local>
2023-03-21 13:04:08 +0700

    LOLOK

--- components/tong-quan/footer.js
+++ components/tong-quan/footer.js
@@ -8,7 +8,7 @@ return (
 
 
     <section className="bg-white overflow-hidden" style={{ backgroundPosition: 'center'}}>
-    <div class="w-full text-center md:w-auto p-3 md:py-0 md:px-6"><Link class="inline-block text-lg md:text-xl text-coolGray-500 hover:text-coolGray-600 font-medium" href="/privacy">Privacy</Link></div>
+    <div className="w-full text-center md:w-auto p-3 md:py-0 md:px-6"><Link className="inline-block text-lg md:text-xl text-coolGray-500 hover:text-coolGray-600 font-medium" href="/privacy">Privacy</Link></div>
         
       </section>
 
