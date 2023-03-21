6bfaa9d912353571f2f92b8c1f5e6ef7691d38fa
Toan Le <toanle@Toans-MacBook-Air.local>
2023-03-21 13:01:38 +0700

    OKEM

--- components/tong-quan/footer.js
+++ components/tong-quan/footer.js
@@ -8,7 +8,7 @@ return (
 
 
     <section className="bg-white overflow-hidden" style={{ backgroundPosition: 'center'}}>
-    <div class="w-full text-center md:w-auto p-3 md:py-0 md:px-6"><a class="inline-block text-lg md:text-xl text-coolGray-500 hover:text-coolGray-600 font-medium" href="/privacy">Privacy</a></div>
+    <div class="w-full text-center md:w-auto p-3 md:py-0 md:px-6"><Link class="inline-block text-lg md:text-xl text-coolGray-500 hover:text-coolGray-600 font-medium" href="/privacy">Privacy</Link></div>
         
       </section>
 
